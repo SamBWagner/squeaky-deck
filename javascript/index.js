c38cec664c6a76a68152da13cd7078d696c78068
Sam Wagner [SSW] <samuel.b.wagner@gmail.com>
2023-08-18 07:54:20 +1000

    Fix styling & add js

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+function cutBottomLine() {
+    let modifyingWindow = document.getElementById("modified").Value;
+
+    console.log("${modifyingWindow}");
+}
+
+function cutTopLine() {
+    let modifyingWindow = document.getElementById("modified").Value;
+
+    console.log("${modifyingWindow}");
+}
+
