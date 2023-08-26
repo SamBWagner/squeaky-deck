07575f6c65f3e3ef365fa71d9686a86d29a5ffbc
Sam Wagner [SSW] <samuel.b.wagner@gmail.com>
2023-08-26 13:57:17 +1000

    remove unused column and add cleansing method

--- javascript/index.js
+++ javascript/index.js
@@ -4,9 +4,28 @@ function loadPageScripts() {
    let inputWindow = document.getElementById("input");
 
     inputWindow.addEventListener("paste", function (event) {
-        let modificationwindow = document.getElementById("modified");
+        let modificationwindow = document.getElementById("output");
 
-        modificationwindow.innerHTML = event.clipboardData.getData("text"); 
+        modificationwindow.innerHTML = cleaseDecklist(event.clipboardData.getData("text"));
     });
+}
+
+function cleaseDecklist(decklist) {
+    let decklistArray = decklist.split('\n');
+    let outputArr = [];
+
+    for (let index = 0; index < decklistArray.length; index++) {
+        const line = decklistArray[index];
+
+        if (!(typeof(line) == 'string')) {
+            continue;
+        }
+
+        if (parseInt(line.charAt(0))) {
+            outputArr.push(line);
+        }
+    }
+
+    return outputArr.join('\n');
 }
 
